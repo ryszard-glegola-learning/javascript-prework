6a1782b87914b4f669cdfd0110a5fa7e095b84c4
Ryszard Glegola <ryszard.glegola@gmail.com>
2019-11-02 14:56:23 +0100

    Added step -- Odczytanie ruchu gracza --

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ruch komputera
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
@@ -11,3 +13,19 @@ if(randomNumber == 1) {
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// Ruch gracza
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(randomNumber == 2) {
+  playerMove = 'papier';
+} else if(randomNumber == 3) {
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
