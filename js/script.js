53637d4b34a6b454d89317193bc13870bf64c1c3
Ryszard Glegola <ryszard.glegola@gmail.com>
2019-11-02 17:02:58 +0100

    Declared functions and used them, played with console.log, all OK

--- js/script.js
+++ js/script.js
@@ -1,57 +1,68 @@
+// Deklaracje funkcji
+
+function getMoveName(argMoveId)
+{
+	if(argMoveId == 1) {
+	  return 'kamień';
+	} else if(argMoveId == 2) {
+	  return 'papier';
+	} else if(argMoveId == 3) {
+	  return 'nożyce';
+	}
+	// printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+	return 'ani kamień, ani papier, ani nożyce';
+}
+
+function displayResult(argComputerMove,argPlayerMove,argPlayerInput)
+{
+	// Zasady: remis – jeśli obaj gracze wybrali ten sam ruch,
+	// wygrana gracza, który zagrał papier, jeśli drugi gracz zagrał kamień,
+	// wygrana gracza, który zagrał kamień, jeśli drugi gracz zagrał nożyce,
+	// wygrana gracza, który zagrał nożyce, jeśli drugi gracz zagrał papier.
+
+	if  (argPlayerMove == 'ani kamień, ani papier, ani nożyce')
+	{
+	  printMessage('Partia nierozstrzygnięta, bo wpisałeś "' + argPlayerInput + '".'); 
+	} 
+	else if (argComputerMove == argPlayerMove) 
+	{
+	  printMessage('Remis!');
+	} 
+	else if ((argComputerMove == 'papier' && argPlayerMove == 'kamień') || 
+			 (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') ||
+			 (argComputerMove == 'nożyce' && argPlayerMove == 'papier'))
+	{
+	  printMessage('Ja wygrywam!');
+	} else {
+	  printMessage('Ty wygrywasz!');
+	}
+}
+
 // Ruch komputera
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1) {
-  computerMove = 'kamień';
-} else if(randomNumber == 2) {
-  computerMove = 'papier';
-} else if(randomNumber == 3) {
-  computerMove = 'nożyce';
-}
-
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Mój ruch to: ' + computerMove + '.');
 
 // Ruch gracza
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisałeś: ' + playerInput);
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if(playerInput == '1') {
-  playerMove = 'kamień';
-} else if(playerInput == 2) {
-  playerMove = 'papier';
-} else if(playerInput == 3) {
-  playerMove = 'nożyce';
-} else {
-  playerMove = 'ani kamień, ani papier, ani nożyce';
-}
-
-printMessage('Twój ruch to: ' + playerInput + ', czyli ' + playerMove);
+printMessage('Twój ruch to: ' + playerInput + ', czyli ' + playerMove + '.');
 
 // Sprawdzenie i podanie wyniku
 
-// Zasady: remis – jeśli obaj gracze wybrali ten sam ruch,
-// wygrana gracza, który zagrał papier, jeśli drugi gracz zagrał kamień,
-// wygrana gracza, który zagrał kamień, jeśli drugi gracz zagrał nożyce,
-// wygrana gracza, który zagrał nożyce, jeśli drugi gracz zagrał papier.
+displayResult(computerMove,playerMove,playerInput);
 
-if  (playerMove == 'ani kamień, ani papier, ani nożyce')
-	{
-  printMessage('Partia nierozstrzygnięta, wpisałeś ' + playerInput + '.'); 
-  } 
-else if 	(computerMove == playerMove) 
-{
-  printMessage('Remis!');
-} 
-else if ((computerMove == 'papier' && playerMove == 'kamień') || 
-		 (computerMove == 'kamień' && playerMove == 'nożyce') ||
-		 (computerMove == 'nożyce' && playerMove == 'papier'))
-{
-  printMessage('Ja wygrywam!');
-} else {
-  printMessage('Ty wygrywasz!');
-}
+// Pobawmy się console.log
+
+console.log('### Sprawdzamy wartości :');
+console.log('Komp wylosował: ', randomNumber);
+console.log('Gracz wpisał: ', playerInput);
+console.log('Co zwraca getMoveName dla 1: ', getMoveName('1'));
+console.log('Co zwraca getMoveName dla 2: ', getMoveName('2'));
+console.log('Co zwraca getMoveName dla 3: ', getMoveName('3'));
+console.log('Co zwraca getMoveName dla ', playerInput, ': ', getMoveName(playerInput));
+console.log(' --- Koniec logu tego uruchomienia --- ');
