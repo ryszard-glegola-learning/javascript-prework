598d62064fbee09d620f94207a1bff4304a1463b
Ryszard Glegola <ryszard.glegola@gmail.com>
2019-11-02 15:26:08 +0100

    Fixed two code errors, tested, it works!

--- js/script.js
+++ js/script.js
@@ -17,15 +17,41 @@ printMessage('Mój ruch to: ' + computerMove);
 // Ruch gracza
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Wpisałeś: ' + playerInput);
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if(playerInput == '1') {
   playerMove = 'kamień';
-} else if(randomNumber == 2) {
+} else if(playerInput == 2) {
   playerMove = 'papier';
-} else if(randomNumber == 3) {
+} else if(playerInput == 3) {
   playerMove = 'nożyce';
+} else {
+  playerMove = 'ani kamień, ani papier, ani nożyce';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerInput + ', czyli ' + playerMove);
+
+// Sprawdzenie i podanie wyniku
+
+// Zasady: remis – jeśli obaj gracze wybrali ten sam ruch,
+// wygrana gracza, który zagrał papier, jeśli drugi gracz zagrał kamień,
+// wygrana gracza, który zagrał kamień, jeśli drugi gracz zagrał nożyce,
+// wygrana gracza, który zagrał nożyce, jeśli drugi gracz zagrał papier.
+
+if  (playerMove == 'ani kamień, ani papier, ani nożyce')
+	{
+  printMessage('Partia nierozstrzygnięta, wpisałeś ' + playerInput + '.'); 
+  } 
+else if 	(computerMove == playerMove) 
+{
+  printMessage('Remis!');
+} 
+else if ((computerMove == 'papier' && playerMove == 'kamień') || 
+		 (computerMove == 'kamień' && playerMove == 'nożyce') ||
+		 (computerMove == 'nożyce' && playerMove == 'papier'))
+{
+  printMessage('Ja wygrywam!');
+} else {
+  printMessage('Ty wygrywasz!');
+}
