a6f31a98fb7c2ae6e9ef42d2fcf0562f8668c514
Ryszard Glegola <ryszard.glegola@gmail.com>
2019-11-04 22:58:45 +0100

    Section 6 Task 1 - Replaced strings with constants.

--- js/script.js
+++ js/script.js
@@ -1,130 +1,140 @@
-function playGame(playerInput) 
 {
+const 	MOVE_ROCK = 'kamień',
+		MOVE_PAPER = 'papier',
+		MOVE_SCISSORS = 'nożyce',
+		WIN_DRAW = 'remis',
+		WIN_PC = 'komp',
+		WIN_PLAYER = 'gracz';
 
-	// Deklaracje funkcji
-	function getMoveName(argMoveId)
+
+	function playGame(playerInput) 
 	{
-		if(argMoveId == 1) {
-		  return 'kamień';
-		} else if(argMoveId == 2) {
-		  return 'papier';
-		} else if(argMoveId == 3) {
-		  return 'nożyce';
-		}
-	}
 
-	function displayResult(argComputerMove,argPlayerMove,argPlayerInput)
-	{
-		// Zasady: remis – jeśli obaj gracze wybrali ten sam ruch,
-		// wygrana gracza, który zagrał papier, jeśli drugi gracz zagrał kamień,
-		// wygrana gracza, który zagrał kamień, jeśli drugi gracz zagrał nożyce,
-		// wygrana gracza, który zagrał nożyce, jeśli drugi gracz zagrał papier.
-
-		if (argComputerMove == argPlayerMove) 
+		// Deklaracje funkcji
+		function getMoveName(argMoveId)
 		{
-		  printMessage('Remis!');
-		  return('remis');
-		} 
-		else if ((argComputerMove == 'papier' && argPlayerMove == 'kamień') || (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'papier'))
-		{
-		  printMessage('Ja wygrywam!');
-		  return('komp');
-		} else {
-		  printMessage('Ty wygrywasz!');
-		  return ('gracz')
+			if(argMoveId == 1) {
+			  return (MOVE_ROCK);
+			} else if(argMoveId == 2) {
+			  return (MOVE_PAPER);
+			} else if(argMoveId == 3) {
+			  return (MOVE_SCISSORS);
+			}
 		}
+
+		function displayResult(argComputerMove,argPlayerMove,argPlayerInput)
+		{
+			// Zasady: remis – jeśli obaj gracze wybrali ten sam ruch,
+			// wygrana gracza, który zagrał papier, jeśli drugi gracz zagrał kamień,
+			// wygrana gracza, który zagrał kamień, jeśli drugi gracz zagrał nożyce,
+			// wygrana gracza, który zagrał nożyce, jeśli drugi gracz zagrał papier.
+
+			if (argComputerMove == argPlayerMove) 
+			{
+			  printMessage('Remis!');
+			  return(WIN_DRAW);
+			} 
+			else if ((argComputerMove == 'papier' && argPlayerMove == 'kamień') || (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'papier'))
+			{
+			  printMessage('Ja wygrywam!');
+			  return(WIN_PC);
+			} else {
+			  printMessage('Ty wygrywasz!');
+			  return (WIN_PLAYER)
+			}
+		}
+
+		// Deklaracja zmiennych
+
+		let winnerCheck = '';
+
+		// Początek algorytmu funkcji
+
+		clearMessages();
+		
+		// Ruch gracza
+
+		let playerMove = getMoveName(playerInput);
+		printMessage('Twój ruch to: ' + playerInput + ', czyli ' + playerMove + '.');
+
+		// Ruch komputera
+
+		let randomNumber = Math.floor(Math.random() * 3 + 1);
+		let computerMove = getMoveName(randomNumber);
+
+		printMessage('Mój ruch to: ' + computerMove + '.');
+
+		// Sprawdzenie i podanie wyniku
+
+		winnerCheck = displayResult(computerMove,playerMove,playerInput);
+
+		// Pobawmy się console.log
+
+		console.log('### Sprawdzamy wartości :');
+		console.log('Komp wylosował: ', randomNumber);
+		console.log('Gracz kliknął: ', getMoveName(playerInput));
+		console.log('Wynik (winnerCheck):', winnerCheck);	
+
+		return(winnerCheck);
 	}
 
-	// Deklaracja zmiennych
+	function buttonClicked(argButtonID, argButtonName, argScoreComputer, argScorePlayer)
+	{
+		let Winner = playGame(argButtonID);
+		clearScore()
+		return(Winner);
+	}
 
-	let winnerCheck = '';
+	// Deklaracje zmiennych
 
-	// Początek algorytmu funkcji
+	let scorePlayerTotal = '0';
+	let scoreComputerTotal = '0';
 
-	clearMessages();
-	
-	// Ruch gracza
+	// Koniec deklaracji, początek właściwego skryptu
 
-	let playerMove = getMoveName(playerInput);
-	printMessage('Twój ruch to: ' + playerInput + ', czyli ' + playerMove + '.');
+	let testButton1 = document.getElementById('play-rock');
+	testButton1.addEventListener('click', function()
+	{
+		let Winner = buttonClicked(1,'Kamień',scorePlayerTotal,scoreComputerTotal);	
+		if (Winner == WIN_PC) 
+		{
+			scoreComputerTotal++;
+		}
+		else if (Winner == WIN_PLAYER) 
+		{
+			scorePlayerTotal++;
+		}
+		printScore(scorePlayerTotal,scoreComputerTotal); 	
+	});
 
-	// Ruch komputera
+	let testButton2 = document.getElementById('play-paper');
+	testButton2.addEventListener('click', function()
+	{
+		let Winner = buttonClicked(2,'Papier',scorePlayerTotal,scoreComputerTotal);	
+		if (Winner == WIN_PC) 
+		{
+			scoreComputerTotal++;
+		}
+		else if (Winner == WIN_PLAYER) 
+		{
+			scorePlayerTotal++;
+		}
+		printScore(scorePlayerTotal,scoreComputerTotal); 	
+	});
 
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
-	let computerMove = getMoveName(randomNumber);
 
-	printMessage('Mój ruch to: ' + computerMove + '.');
-
-	// Sprawdzenie i podanie wyniku
-
-	winnerCheck = displayResult(computerMove,playerMove,playerInput);
-
-	// Pobawmy się console.log
-
-	console.log('### Sprawdzamy wartości :');
-	console.log('Komp wylosował: ', randomNumber);
-	console.log('Gracz kliknął: ', getMoveName(playerInput));
-	console.log('Wynik (winnerCheck):', winnerCheck);	
-
-	return(winnerCheck);
+	let testButton3 = document.getElementById('play-scissors');
+	testButton3.addEventListener('click', function()
+	{
+		let Winner = buttonClicked(3,'Nożyce',scorePlayerTotal,scoreComputerTotal);	
+		if (Winner == WIN_PC) 
+		{
+			scoreComputerTotal++;
+		}
+		else if (Winner == WIN_PLAYER) 
+		{
+			scorePlayerTotal++;
+		}
+		printScore(scorePlayerTotal,scoreComputerTotal); 	
+	});
 }
-
-function buttonClicked(argButtonID, argButtonName, argScoreComputer, argScorePlayer)
-{
-	let Winner = playGame(argButtonID);
-	clearScore()
-	return(Winner);
-}
-
-// Deklaracje zmiennych
-
-let scorePlayerTotal = '0';
-let scoreComputerTotal = '0';
-
-// Koniec deklaracji, początek właściwego skryptu
-
-let testButton1 = document.getElementById('play-rock');
-testButton1.addEventListener('click', function()
-{
-	let Winner = buttonClicked(1,'Kamień',scorePlayerTotal,scoreComputerTotal);	
-	if (Winner == 'komp') 
-	{
-		scoreComputerTotal++;
-	}
-	else if (Winner == 'gracz') 
-	{
-		scorePlayerTotal++;
-	}
-	printScore(scorePlayerTotal,scoreComputerTotal); 	
-});
-
-let testButton2 = document.getElementById('play-paper');
-testButton2.addEventListener('click', function()
-{
-	let Winner = buttonClicked(2,'Papier',scorePlayerTotal,scoreComputerTotal);	
-	if (Winner == 'komp') 
-	{
-		scoreComputerTotal++;
-	}
-	else if (Winner == 'gracz') 
-	{
-		scorePlayerTotal++;
-	}
-	printScore(scorePlayerTotal,scoreComputerTotal); 	
-});
-
-
-let testButton3 = document.getElementById('play-scissors');
-testButton3.addEventListener('click', function()
-{
-	let Winner = buttonClicked(3,'Nożyce',scorePlayerTotal,scoreComputerTotal);	
-	if (Winner == 'komp') 
-	{
-		scoreComputerTotal++;
-	}
-	else if (Winner == 'gracz') 
-	{
-		scorePlayerTotal++;
-	}
-	printScore(scorePlayerTotal,scoreComputerTotal); 	
-});
